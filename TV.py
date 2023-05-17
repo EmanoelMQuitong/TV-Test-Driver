d58a05135f2662e0bb424e1b4efe35cbe6832692
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-05-17 12:04:53 +0800

    Tv and Tv test driver has successfully listed functions

---
+++ TV.py
@@ -0,0 +1,16 @@
+
+class TV():
+    def __init__(tv, power, channel, volume):
+        tv.power = power
+        tv.channel = channel
+        tv.volume = volume
+                 
+    def turnOn(): None
+    def turnOff(): None
+    def getChannel(channel: int): None
+    def getVolume(): int
+    def setVolume(volumeLevel: int): None
+    def channelUp(): None
+    def channelDown(): None
+    def volumeUp(): None
+    def volumeDown(): None
---
+++ Test Driver.py
@@ -0,0 +1,8 @@
+from TV import TV
+
+def main():
+    channel : int
+    volumeLevel: int
+    on: bool
+
+
