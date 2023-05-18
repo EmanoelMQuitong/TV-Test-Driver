bf535c05ae803825a569e9d700cee0ffc2346846
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-05-19 01:26:09 +0800

    Get channel has been successful in the Test Driver.py File exception has been added Change channel while loop has been added. Get volume basic function has been successfully added.

--- TV.py
+++ TV.py
@@ -11,7 +11,7 @@ END = '\033[0m'
 
 
 class TV():
-    def __init__(tv, power, channel = 1, volume = 1):
+    def __init__(tv, power = 'Off', channel = 1, volume = 1):
         tv.power = power
         tv.channel = channel
         tv.volume = volume
@@ -77,35 +77,17 @@ class TV():
 
 #Decreases Channel number by 1
     def channelDown(tv):
-        return int(channel) - 1
+        return int(tv.channel) - 1
 
 #Increases Volume number by 1
     def volumeUp(tv): 
-        return int(volume) + 1
+        return int(tv.volume) + 1
 
 #Decreases Volume number by 1
     def volumeDown(tv): 
-        return int(volume) - 1
-    
+        return int(tv.volume) - 1
     def show(tv):
         print(tv.power, tv.channel, tv.volume)
-        print(tv.power, channel, volume)
-        print(tv.turnOn())
-        print(tv.turnOff())
-        print("Set channel result", tv.setChannel())
-        newChannel = tv.getChannel() 
-        print("Get channel result", newChannel)
-        print("Get volume result", tv.getVolume())
-        newVolume = tv.setVolume()
-        print("Set volume result", newVolume)
-        print("Volume down result", tv.volumeDown())
-        print("Volume Up result", tv.volumeUp())
-        print("Channel down result", tv.channelDown())
-        print("Channel Up result", tv.channelUp())
 
-power = input("Would you like to turn the Television 'On' or 'Off'?: ")
-channel = input("Enter channel: ")
-volume = input("Kindly enter volume: ")
-
-Tv1 = TV(power ,  channel, volume)
+Tv1 = TV()
 Tv1.show()
--- Test Driver.py
+++ Test Driver.py
@@ -1,7 +1,49 @@
 from TV import TV
+#Colors
+BLUE = '\033[94m'
+DARKCYAN = '\033[36m'
+PURPLE = '\033[95m'
+GREEN = '\033[92m'
+RED = '\033[91m'
+BOLD = '\033[1m'
+END = '\033[0m'
+BORDER = 170*'='
+BORDER = print(GREEN+BOLD+BORDER+END)
 
 def main():
-    Tv1 = TV('On', 15, 15)
-    print(Tv1.power)
-    print(Tv1.channel, Tv1.setVolume, Tv1.setChannel)
+    Tv1 = TV()
+    response = input("Would you to turn the Television 1 'On' or 'Off'?:  ")
+    if response == Tv1.turnOn():
+        ('/n')
+        
+        #Get channel
+        channel = input("What channel do you want to watch?(Choose from 1-120 only): ")
+        channel = Tv1.getChannel(channel)
+        ask_change_chan = input("Would you like to change the channel?('yes' or 'no') ")
+        
+        ('/n')
+        #Change Channel
+        if ask_change_chan.lower() == 'yes':
+            while ask_change_chan.lower() == 'yes':
+                New_channel = Tv1.setChannel()
+                ask_change_chan = input("Would you like to change the channel?('yes' or 'no'): ")
+                ('/n')
+            channel = New_channel
 
+        elif ask_change_chan.lower() == 'no':
+            pass
+
+        else:
+            raise Exception(RED +"Please choose between 'yes' or 'no' only."+ END)
+
+        ('/n')
+
+        #Get Volume
+        volumeLevel = input("What is your desired volume Level? (Choose from 1-7 only): ")
+        volume = Tv1.getVolume(volumeLevel)
+
+    elif response == Tv1.turnOff():
+        print("Television 1 is Off.")
+    else:
+        raise Exception ("Please answer 'On' or 'Off' only.") 
+        
