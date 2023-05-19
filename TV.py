66a1c3ea1b40b7b707e65a249b5033778c229377
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-05-19 12:07:13 +0800

    Object 1: Television 1 has been successfully created.

--- TV.py
+++ TV.py
@@ -12,26 +12,36 @@ END = '\033[0m'
 color = fg('blue')
 BORDER = 175*'='
 
-print(GREEN+BOLD+BORDER+END)
+
 
 
 class TV():
-    def __init__(tv, power = 'Off', channel = 1, volume = 1):
+    def __init__(tv, power = False, channel = 1, volume = 1):
         tv.power = power
         tv.channel = channel
         tv.volume = volume
 
 #Turns On tv              
     def turnOn(tv):
-        tv.power = 'On'
-        return tv.power
-            
+        response = input("Would you like to turn the power 'On' or 'Off'?: ")
+        tv.power = response
+        if tv.power == 'On':
+            tv.power = True
+            return tv.power
+        elif tv.power == 'Off':
+            tv.power = False
+            return tv.power
+        else:
+            raise Exception(RED + "Please choose between 'On' or 'Off' only."+ END)
+        ('/n')    
             
         
 #Turns off Tv.
     def turnOff(tv):
-        tv.power = 'Off'
-        return tv.power
+        if tv.power == False:
+            quit()
+        else:
+            pass
             
                              
 #Returns Channel for Tv.
@@ -39,38 +49,47 @@ class TV():
         tv.channel = int(input("What channel do you want to watch?(Choose from 1-120 only): "))
         if tv.channel < 120:
             if tv.channel >= 1:
-                return int(tv.channel)
+                tv.channel = int(tv.channel)
+                ('/n')
+                return tv.channel
             else: 
                 raise Exception(RED + "Sorry, no numbers below zero (0)."+ END)
         else:
             raise Exception (RED + "Sorry, no numbers greater than 120"+ END)
-        
+       
+
 #Set channel a new Channel for Tv.
     def setChannel(tv):
+        
         response = input("Would you like to enter a NEW CHANNEL?('yes'/'no'): ")
         if response == 'yes':
+            tv.channel = ''
             while response == 'yes':
+                print('\n')
                 tv.channel = input("Please enter NEW CHANNEL: ")
                 if int(tv.channel) < 120:
                     if int(tv.channel) >= 1:
-                        response = input("Would you like to enter a NEW CHANNEL?('yes'/'no')")
+                        print('\n')
+                        response = input("Would you like to enter a NEW CHANNEL?('yes'/'no'): ")                       
                     else: 
                         raise Exception(RED + "Sorry, no numbers below zero (0)."+ END)
                 else:
                     raise Exception (RED + "Sorry, no numbers greater than 120"+ END)
-            
-            return int(tv.channel)
+            tv.channel = int(tv.channel)
+            return tv.channel
         
         elif response.lower() == 'no':
             pass
         else:
             raise Exception(RED +"Please choose between 'yes' or 'no' only."+ END)
+        ('/n')
 
 #Returns Channel for Tv.
     def getVolume(tv):
-        tv.volume = int(input("What channel do you want to watch?(Choose from 1-7 only): ")) 
+        tv.volume = int(input("What is your desired volume level?(Choose from 1-7 only): ")) 
         if tv.volume < 8:
             if tv.volume >= 1:
+                ('/n')
                 return int(tv.volume)
             else: 
                 raise Exception(RED + "Sorry, no numbers below zero (0)."+ END)
@@ -78,20 +97,25 @@ class TV():
             raise Exception (RED + "Sorry, no numbers greater than 7"+ END)
         
 #Set channel a new Volume for Tv.
-    def setVolume(tv): 
-        response = input("Would you like to enter a NEW CHANNEL?('yes'/'no')")
+    def setVolume(tv):
+        print('\n') 
+        response = input("Would you like to enter a NEW VOLUME?('yes'/'no'): ")
         if response == 'yes':
+            tv.volume = ''
             while response == 'yes':
-                new_volume = input("Please enter NEW CHANNEL: ")
-                if int(new_volume) < 120:
-                    if int(new_volume) >= 1:
-                        response = input("Would you like to enter a NEW CHANNEL?('yes'/'no')")
+                print('\n')
+                tv.volume = input("Please enter NEW VOLUME LEVEL: ")
+                if int(tv.volume) < 7:
+                    if int(tv.volume) >= 1:
+                        print('\n')
+                        response = input("Would you like to enter a NEW VOLUME VOLUME LEVEL?('yes'/'no'): ")
+
                     else: 
                         raise Exception(RED + "Sorry, no numbers below zero (0)."+ END)
                 else:
-                    raise Exception (RED + "Sorry, no numbers greater than 120"+ END)
+                    raise Exception (RED + "Sorry, no numbers greater than 7"+ END)
             
-            return int(new_volume)
+            return int(tv.volume)
         
         elif response.lower() == 'no':
             pass
@@ -104,9 +128,10 @@ class TV():
         response = input("Would you like to increase the channel by one (1) ? ('yes'/'no'): ") 
         if response == 'yes':
             while response == 'yes':
-                int(tv.channel) + 1
+                tv.channel = int(tv.channel) + 1
                 response = input("Would you like to increase the channel by one (1) ? ('yes'/'no'): ")
-            
+                ('/n')
+
             return int(tv.channel)
         elif response.lower() == 'no':
             pass
@@ -118,9 +143,10 @@ class TV():
         response = input("Would you like to decrease the channel by one (1) ? ('yes'/'no'): ") 
         if response == 'yes':
             while response == 'yes':
-                int(tv.channel) - 1
+                tv.channel = int(tv.channel) - 1
                 response = input("Would you like to decrease the channel by one (1) ? ('yes'/'no'): ")
-            
+                ('/n')
+
             return int(tv.channel)
         elif response.lower() == 'no':
             pass
@@ -132,9 +158,10 @@ class TV():
         response = input("Would you like to increase the volume by one (1) ? ('yes'/'no'): ") 
         if response == 'yes':
             while response == 'yes':
-                int(tv.volume) + 1
+                tv. volume = int(tv.volume) + 1
                 response = input("Would you like to increase the volume by one (1) ? ('yes'/'no'): ")
-            
+                ('/n')
+
             return int(tv.volume)
         elif response.lower() == 'no':
             pass
@@ -146,9 +173,10 @@ class TV():
         response = input("Would you like to decrease the volume by one (1) ? ('yes'/'no'): ") 
         if response == 'yes':
             while response == 'yes':
-                int(tv.volume) - 1
+                tv. volume = int(tv.volume) - 1
                 response = input("Would you like to decrease the volume by one (1) ? ('yes'/'no'): ")
-            
+                ('/n')
+
             return int(tv.volume)
         elif response.lower() == 'no':
             pass
--- Test Driver.py
+++ Test Driver.py
@@ -17,6 +17,46 @@ BOLD = '\033[1m'
 END = '\033[0m'
 BORDER = 175*'='
 BORDER = print(GREEN+BOLD+BORDER+END)
+print(GREEN+BOLD+BORDER+END)
 
-def main():
-    name = input("pls enter name")
+text_print = (f"Television 1")
+print(color + pyfiglet.figlet_format(text_print , font= "bubble"))
+print('\n') 
+Tv1 = TV()
+Tv1.turnOn()
+Tv1.turnOff()
+
+Tv1.getChannel()
+print(BLUE +"Current Channel: "+ END, Tv1.channel)
+print('\n')
+Tv1.setChannel()
+print(BLUE +"Current Channel: "+ END, Tv1.channel)
+print('\n')
+
+Tv1.getVolume()
+print(BLUE +"Current Volume: "+ END, Tv1.volume)
+print('\n')
+Tv1.setVolume()
+print(BLUE +"Current Volume: "+ END, Tv1.volume)
+print('\n')
+
+Tv1.channelUp()
+print(BLUE +"Current Channel: "+ END, Tv1.channel)
+print('\n')
+Tv1.channelDown()
+print(BLUE +"Current Channel: "+ END, Tv1.channel)
+print('\n')
+
+Tv1.volumeUp()
+print(BLUE +"Current Volume: "+ END, Tv1.volume)
+print('\n')
+Tv1.volumeDown()
+print(BLUE +"Current Volume: "+ END, Tv1.volume)
+print('\n')
+
+print(PURPLE +"Final Channel: "+ END, Tv1.channel)
+print(PURPLE +"Final Volume Level: "+ END, Tv1.volume)
+print(PURPLE +"Is it on?: "+ END, Tv1.power)
+
+print(GREEN+BOLD+BORDER+END)
+print(GREEN+BOLD+BORDER+END)
